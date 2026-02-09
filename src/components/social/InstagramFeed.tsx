f33aff429d828faf2327e8d4b1e00ec3429b02f7
ReBrand House <rebrandhouse@ReBrands-MacBook-Pro.local>
2026-02-08 20:02:23 -0600

    Revert to standard Instagram Embed (remove custom feed & db config)

--- src/components/social/InstagramFeed.tsx
+++
@@ -1,8 +0,0 @@
-import { getInstagramPosts } from "@/lib/instagram";
-import { InstagramFeedClient } from "./InstagramFeedClient";
-
-export async function InstagramFeed() {
-    const posts = await getInstagramPosts();
-
-    return <InstagramFeedClient posts={posts} />;
-}
